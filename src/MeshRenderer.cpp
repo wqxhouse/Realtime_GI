ccc6861eed13679297eb8306df23a0374359b4a4
wqxhouse <wqxhouse1@gmail.com>
2015-11-13 12:20:51 -0800

    Lower shadow quality for debug mode to increase performance

--- src/MeshRenderer.cpp
+++ src/MeshRenderer.cpp
@@ -21,7 +21,20 @@
 #include "AppSettings.h"
 #include "SharedConstants.h"
 
-// Constants
+// Constants - low shadow quality in debug mode to speed up
+#if _DEBUG
+static const float ShadowNearClip = 1.0f;
+static const float FilterSize = 0.0f;
+static const uint32 SampleRadius = 0;
+static const float OffsetScale = 0.0f;
+static const float LightBleedingReduction = 0.10f;
+static const float PositiveExponent = 40.0f;
+static const float NegativeExponent = 8.0f;
+static const uint32 ShadowMapSize = 512;
+static const uint32 ShadowMSAASamples = 1;
+static const uint32 ShadowAnisotropy = 16;
+static const bool EnableShadowMips = false;	
+#else 
 static const float ShadowNearClip = 1.0f;
 static const float FilterSize = 7.0f;
 static const uint32 SampleRadius = 3;
@@ -33,6 +46,7 @@ static const uint32 ShadowMapSize = 1024;
 static const uint32 ShadowMSAASamples = 4;
 static const uint32 ShadowAnisotropy = 16;
 static const bool EnableShadowMips = true;
+#endif
 
 void MeshRenderer::LoadShaders()
 {
