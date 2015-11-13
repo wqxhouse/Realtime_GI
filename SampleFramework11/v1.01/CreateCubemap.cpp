b892891a13ab79fd16a9cd44708d6ad733e70088
Bei XiangShun <bxs3514@hotmail.com>
2015-11-13 14:21:06 -0800

    Start to build cubemap generator.

---
+++ SampleFramework11/v1.01/CreateCubemap.cpp
@@ -0,0 +1,26 @@
+#include "CreateCubemap.h"
+
+
+VOID CreateCubemap::Initialize(){
+
+}
+
+
+VOID CreateCubemap::SetPosition(Float3 position){
+
+}
+
+
+VOID CreateCubemap::Create(){
+
+}
+
+
+CONST FirstPersonCamera &CreateCubemap::GetFirstPersonCamera(){
+	return cubemapCamera;
+}
+
+
+CreateCubemap::~CreateCubemap()
+{
+}
---
+++ SampleFramework11/v1.01/CreateCubemap.h
@@ -0,0 +1,28 @@
+/*
+	Author : bxs3514
+	Date   : 2015.11
+*/
+
+#pragma once
+
+#include <PCH.h>
+#include <Graphics\Camera.h>
+
+using namespace SampleFramework11;
+
+class CreateCubemap
+{
+private:
+	FirstPersonCamera cubemapCamera;//Camera used to catch scenes in 6 directions
+	D3D11_TEXTURE2D_DESC cubemapTex;
+	D3D11_SUBRESOURCE_DATA cubemapData;
+	std::vector<XMFLOAT4> cubemapColor[6];//RGBA color buffer of cubemap faces
+	Float3 Position;
+public:
+	VOID Initialize();
+	VOID SetPosition(Float3 position);
+	CONST FirstPersonCamera &GetFirstPersonCamera();
+	VOID Create();
+	~CreateCubemap();
+};
+
