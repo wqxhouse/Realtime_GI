aa846f41d352563a2b62138d20da8ef337dec14e
Bei XiangShun <bxs3514@hotmail.com>
2015-11-13 16:52:19 -0800

    Initialize constant variables, some other modifies
    Define constant variables of Cubemap.
    Modify some variable type to fit framework.
    Move createcubemap class out of framework folder.
    *Next: Initialize Rendertarget2D

--- SampleFramework11/v1.01/CreateCubemap.h
+++ SampleFramework11/v1.01/CreateCubemap.h
@@ -6,22 +6,35 @@
 #pragma once
 
 #include <PCH.h>
-#include <Graphics\Camera.h>
+#include <Graphics\\Camera.h>
+#include <Graphics\\GraphicsTypes.h>
 
 using namespace SampleFramework11;
 
 class CreateCubemap
 {
 private:
+	const uint32 WindowWidth = 128;
+	const uint32 WindowHeight = 128;
+
 	PerspectiveCamera cubemapCamera;//Camera used to catch scenes in 6 directions
-	D3D11_TEXTURE2D_DESC cubemapTex;
-	D3D11_SUBRESOURCE_DATA cubemapData;
-	std::vector<XMFLOAT4> cubemapColor[6];//RGBA color buffer of cubemap faces
-	Float3 Position;
+	//D3D11_TEXTURE2D_DESC cubemapTex;
+	//D3D11_SUBRESOURCE_DATA cubemapData;
+	//std::vector<XMFLOAT4> cubemapColor[6];//RGBA color buffer of cubemap faces
+	RenderTarget2D cubemapTarget;
+	Float3 position;
+	struct CameraStruct{
+		Float3 Eye;
+		Float3 LookAt;
+		Float3 Up;
+	}CubemapCameraStruct[6];
+	static const CameraStruct DefaultCubemapCameraStruct[6];
+	static const Float3 DefaultPosition;
 public:
+	CreateCubemap(const float NearClip, const float FarClip);
 	VOID Initialize();
 	VOID SetPosition(Float3 position);
-	CONST PerspectiveCamera &GetFirstPersonCamera();
+	CONST PerspectiveCamera &GetCubemapCamera();
 	VOID Create();
 	~CreateCubemap();
 };
